bd646e44317da6f95530be93665af9641b33f37c
SirBatatinha <68497472+SirBatatinha@users.noreply.github.com>
2022-05-09 08:14:29 -0300

    melhorando o codigo

--- form.js
+++ form.js
@@ -1,97 +1,95 @@
 var accounts = [];
-function usernameIsValid(){
-    let name = document.forms["cadastro"]["nome"].value;
-    let input = document.getElementById("nome");
-    if(name.length < 3 || name.length > 25){
-        input.className= "erro";
+function usernameIsValid() {
+    const name = document.forms["cadastro"]["nome"];
+    if (name.value.length < 3 || name.value.length > 25) {
+        name.className = "erro";
         document.getElementById("alertNome").innerHTML = "Nome deve ter de 3 a 25 caracteres";
-    }else{ 
-        input.className= "certo";
+    } else {
+        name.className = "certo";
         document.getElementById("alertNome").innerHTML = "";
     }
     buttonIsDisabled();
 }
-function emailIsValid(){
-    let email = document.forms["cadastro"]["email"].value;
-    let input = document.getElementById("email");
-    if(email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
-        input.className= "certo";
+function emailIsValid() {
+    const email = document.forms["cadastro"]["email"];
+    if (email.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        email.className = "certo";
         document.getElementById("alertEmail").innerHTML = "";
-    }else{
-        input.className = "erro";
-        document.getElementById("alertEmail").innerHTML = "Insira um email valido";
+    } else {
+        email.className = "erro";
+        document.getElementById("alertEmail").innerHTML ="Insira um email valido";
     }
     buttonIsDisabled();
 }
-function passwordIsValid(){
-    let password = document.forms["cadastro"]["senha"].value;
-    let cPassword = document.forms["cadastro"]["Csenha"].value;
-    let inputSenha = document.getElementById("senha");
-    let inputConf = document.getElementById("Csenha");
+function passwordIsValid() {
+    const password = document.forms["cadastro"]["senha"];
 
-    if(password.length < 8){
-        inputSenha.className = "erro";
+    if (password.value.length < 8) {
+        password.className = "erro";
         document.getElementById("alertSenha").innerHTML = "Senha muito curta";
-    }else{
-        inputSenha.className = "certo";
+    } else {
+        password.className = "certo";
         document.getElementById("alertSenha").innerHTML = "";
     }
+    confirmPasswordIsValid();
+}
+function confirmPasswordIsValid() {
+    const password = document.forms["cadastro"]["senha"].value;
+    const cPassword = document.forms["cadastro"]["Csenha"];
 
-    if(cPassword !== "" && cPassword !== password){
-        inputConf.className = "erro";
-        document.getElementById("alertCsenha").innerHTML = "O valor deve ser iagua a senha";
-    }else if(cPassword !== ""){
-        inputConf.className = "certo";
+    if (cPassword.value !== password) {
+        cPassword.className = "erro";
+        document.getElementById("alertCsenha").innerHTML = "O valor deve ser igual a senha";
+    } else if (cPassword.value !== "") {
+        cPassword.className = "certo";
         document.getElementById("alertCsenha").innerHTML = "";
     }
     buttonIsDisabled();
-    
 }
 
-function validateForm(){
-    document.forms["cadastro"]["submit"].disabled = true
-    setTimeout(() => {document.forms["cadastro"]["submit"].disabled = false}, 1000);
-    const name = document.forms["cadastro"]["nome"].value;
-    const email = document.forms["cadastro"]["email"].value;
-    const inputName = document.getElementById("nome");
-    const inputEmail = document.getElementById("email");
+function validateForm() {
+    document.getElementById("submit").disabled = true;
+    setTimeout(() => {
+        document.getElementById("submit").disabled = false;
+    }, 1000);
+    const name = document.forms["cadastro"]["nome"];
+    const email = document.forms["cadastro"]["email"];
 
     let isValid = true;
     accounts.forEach(account => {
-        if(account.name === name){
+        if (account.name === name.value) {
             isValid = false;
             document.getElementById("alertNome").innerHTML = "Nome ja registrado";
-            inputName.className = "erro";
+            name.className = "erro";
         }
     });
     accounts.forEach(account => {
-        if(account.name === name){
+        if (account.email === email.value) {
             isValid = false;
             document.getElementById("alertEmail").innerHTML = "Email ja registrado";
-            inputEmail.className = "erro";
+            email.className = "erro";
         }
     });
-    if(isValid){
+    if (isValid) {
         accounts[accounts.length] = {
-            name: name,
-            email: email,
-            password: document.forms["cadastro"]["senha"].value
+            name: name.value,
+            email: email.value,
+            password: document.forms["cadastro"]["senha"].value,
         };
     }
 }
 
-function buttonIsDisabled(){
-    let inputSenha = document.getElementById("senha");
-    let inputConf = document.getElementById("Csenha");
-    let inputNome = document.getElementById("nome");
-    let email = document.getElementById("email");
-    if(
-        inputSenha.className === "certo" && 
-        inputConf.className === "certo" &&  
-        inputNome.className === "certo" &&  
+function buttonIsDisabled() {
+    const password = document.forms["cadastro"]["senha"];
+    const cPassword = document.forms["cadastro"]["Csenha"];
+    const name = document.forms["cadastro"]["nome"];
+    const email = document.forms["cadastro"]["email"];
+    if (
+        password.className === "certo" &&
+        cPassword.className === "certo" &&
+        name.className === "certo" &&
         email.className === "certo"
-    ){
+    ) {
         document.getElementById("submit").disabled = false;
-    }else 
-        document.getElementById("submit").disabled = true;
+    } else document.getElementById("submit").disabled = true;
 }
